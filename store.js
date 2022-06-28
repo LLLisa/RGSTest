1d1752b48cf10744e9aa467f4571c8e78d625400
Lisa Knox <lisaknox83@gmail.com>
2022-06-28 15:31:39 -0400

    fixed dependency bug

--- store.js
+++ store.js
@@ -1,14 +1,13 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-// import GeneralStore from 'redux-general-store';
-import GeneralStore from "/home/lisa/Code/recreational-code/ReduxGeneralStore/package/index.js";
+import GeneralStore from 'redux-general-store';
 
-export const GS = new GeneralStore("http://localhost:9001", [
-  "users",
-  "accounts",
-  "files",
+export const GS = new GeneralStore('http://localhost:9001', [
+  'users',
+  'accounts',
+  'files',
 ]);
 
 const reducer = combineReducers({
